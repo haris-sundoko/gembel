e4a6cec8f6dff2be7129e96222565b615c5b2f22
kharismagp <gilang230@gmail.com>
2020-10-28 21:17:44 +0700

    Change default english language to bahasa indonesia

--- space-location.js
+++ space-location.js
@@ -74,5 +74,5 @@ app.post('/delete',(req, res) => {
  
 //server listening
 app.listen(8000, () => {
-  console.log('Server is running at port 8000');
+  console.log('Server berjalan pada port 8000');
 });
